f4a985dd34e13327d5aa4df1e6903b237ad21483
JR Cala <jrcala7@gmail.com>
2026-03-26 10:02:19 +0800

    Add project files.

---
+++ Weeb_Big2.cpp
@@ -0,0 +1,12 @@
+﻿// Weeb_Big2.cpp : Defines the entry point for the application.
+//
+
+#include "Weeb_Big2.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
